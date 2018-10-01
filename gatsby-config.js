4abb26d53479eee1f7bba3e7da30f29bafaf0149
Filip Havrlent <havrlent@havrlentmac.local>
2018-10-02 00:11:07 +0200

    Init commit

---
+++ gatsby-config.js
@@ -0,0 +1,37 @@
+if (process.env.ENVIROMENT !== 'production') {
+  require('dotenv').config()
+}
+
+const contentfulConfig = {
+  spaceId: process.env.SPACE_ID,
+  accessToken: process.env.ACCESS_TOKEN,
+}
+
+module.exports = {
+  siteMetadata: {
+    title: 'Gatsby Contentful TypeScript starter',
+  },
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: 'gatsby-starter-contentful-typescript',
+        short_name: 'starter',
+        start_url: '/',
+        background_color: '#663399',
+        theme_color: '#663399',
+        display: 'minimal-ui',
+        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+      },
+    },
+    'gatsby-plugin-offline',
+    `gatsby-plugin-typescript`,
+    'gatsby-transformer-remark',
+    'gatsby-plugin-sharp',
+    {
+      resolve: `gatsby-source-contentful`,
+      options: contentfulConfig,
+    },
+  ],
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+
+class IndexPage extends Component {
+  render() {
+    return (
+      <Layout>
+        <h1>Hi people</h1>
+        <p>Welcome to your new Gatsby site.</p>
+        <p>Now go build something great.</p>
+        <Link to="/page-2/">Go to page 2</Link>
+      </Layout>
+    )
+  }
+}
+export default IndexPage
+
+
